857bc32410ebed3cba9dfd6a0fa8e85cf5820b9e
Shubham Gupta <shubhamgupta083@gmail.com>
2025-11-18 05:44:45 +0530

    fixed minio

--- infra/terraform/modules/storage/main.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "install_minio" {
-    provisioner "local-exec" {
-    command = "powershell.exe -ExecutionPolicy Bypass -File ${path.module}\\install-minio.ps1 -ChartPath ${path.root}\\charts\\minio"
-
-    }
-}
